18bee62be0e850174df87ff4e9d43b673c03ea00
Khushi M <38930168+Sin317@users.noreply.github.com>
2024-02-14 17:02:13 -0500

    book form is working

---
+++ src/Bookform.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+export function Bookform({onSubmit}) {
+    const [book, setBook] = useState({
+        title: '',
+        author: '',
+        date: '',
+        pages: 0
+    });
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(book);
+        onSubmit(book);
+        setBook({title: '', author: '', date:'', pages: 0})
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setBook(prevBook => ({
+            ...prevBook,
+            [name]: value
+        }));
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit} className="new-item-form">
+                <label htmlFor="title">Book Title</label>
+                <input
+                    id="title"
+                    type="text"
+                    name="title"
+                    value={book.title}
+                    onChange={handleChange}
+                    required
+                />
+                <label htmlFor="author">Author</label>
+                <input
+                    id="author"
+                    type="text"
+                    name="author"
+                    value={book.author}
+                    onChange={handleChange}
+                    required
+                />
+                <label htmlFor="date">Published Date</label>
+                <input
+                    id="date"
+                    type="date"
+                    name="date"
+                    value={book.date}
+                    onChange={handleChange}
+                    required
+                />
+                <label htmlFor="pages">Pages read</label>
+                <input
+                    id="pages"
+                    type="number"
+                    name="pages"
+                    value={book.pages}
+                    onChange={handleChange}
+                    required
+                />
+                <button type="submit" className="btn">Submit</button>
+            </form>
+        </div>
+    );
+}
